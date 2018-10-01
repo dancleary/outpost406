79818106bad980f3fcc52e9b3796b1713818efd4
Daniel Cleary <danieljcleary1@gmail.com>
2018-09-30 20:17:08 -0700

    more cleanup

--- js/index.js
+++ js/index.js
@@ -5,14 +5,31 @@ var navbar;
 var divider;
 var sticky;
 var nav_bg;
+var fading_in = false;
+var fading_out = false;
+
 setValues();
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
 	console.log(sticky + " : " + window.pageYOffset);
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    if(fading_in == false){
+    		fading_in = true;
+    		fading_out = false;
+    		$('#return-to-top').fadeIn(500, function(){
+    			fading_in = false;
+    		});
+	}
   }else {
     navbar.classList.remove("sticky");
+    if(fading_out == false){
+    		fading_out = true;
+    		fading_in = false;
+    		$('#return-to-top').fadeOut(500, function(){
+    			fading_out = false;
+    		});
+	}
   }
   if(window.pageYOffset >= nav_bg){
   	navbar.classList.add("nav-bg")
@@ -52,4 +69,16 @@ $('#foodBtn').on('click', function(){
 	$('#food-menu').fadeIn();
 	$(this).addClass('underlined');
 	$('#drinkBtn').removeClass('underlined')
+});
+
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 600);
+});
+
+$('.nav-link').on('click', function(){
+		$('body,html').animate({
+        		scrollTop : $($(this).attr('data')).offset().top                      // Scroll to top of body
+    		}, 600);
 });
